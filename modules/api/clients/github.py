c30d79d698cc33fb0fa91ce78b66e0f136cebc20
Yuliia Kosminska <y.kosm@ukr.net>
2023-07-25 22:06:51 +0300

    Commit for part 4

---
+++ modules/api/clients/github.py
@@ -0,0 +1,24 @@
+import requests
+
+class GitHub:
+
+    def get_user(self, username):
+        r = requests.get(f'http://api.github.com/users/{username}')
+        body = r.json()
+        return body
+    
+    def search_repo(self, name):
+        r = requests.get("https://api.github.com/search/repositories", params = {"q": name})
+        body = r.json()
+        return body
+
+
+#    def get_user_defunkt(self):
+#        r = requests.get('http://api.github.com/users/defunkt')
+#        body = r.json()
+#        return body
+    
+#    def get_non_exist_user(self):
+#        r = requests.get('http://api.github.com/users/butenkosergii')
+#        body = r.json()
+#        return body
